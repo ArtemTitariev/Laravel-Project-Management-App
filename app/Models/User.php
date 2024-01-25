b15851748285f43f46fad0424909a674849ff192
Artem Titariev <artem.titariev@gmail.com>
2024-01-25 18:08:09 +0200

    instal filament, change field [first_name] to [name] in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'first_name', 'second_name',
+        'name', 'second_name',
         'email', 'password',
         'avatar',
         //'is_active',
