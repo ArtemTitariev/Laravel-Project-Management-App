fe309221f347b2f71225b32d6e7b3f2b5b7861b6
Artem Titariev <artem.titariev@gmail.com>
2024-01-26 21:00:45 +0200

    fix mistake in column name (tasks table)

--- database/migrations/2024_01_22_140839_create_tasks_table.php
+++ database/migrations/2024_01_22_140839_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('descriprion');
+            $table->string('description');
 
             $table->unsignedBigInteger('project_id');
             $table->unsignedBigInteger('category_id');
