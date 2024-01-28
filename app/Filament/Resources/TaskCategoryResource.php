e2ba4b99f6026ec22e589dd5687371288332edf5
Artem Titariev <artem.titariev@gmail.com>
2024-01-28 14:09:30 +0200

    implement small additional resources: position, user role, categories, statuses

---
+++ app/Filament/Resources/TaskCategoryResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use App\Models\TaskCategory;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\TaskCategoryResource\Pages;
+use App\Filament\Resources\TaskCategoryResource\RelationManagers;
+
+class TaskCategoryResource extends Resource
+{
+    protected static ?string $model = TaskCategory::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->sortable()->searchable()
+                    ->wrap(),
+                TextColumn::make('created_at')
+                    ->date()
+                    ->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTaskCategories::route('/'),
+            'create' => Pages\CreateTaskCategory::route('/create'),
+            'edit' => Pages\EditTaskCategory::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/TaskStatusResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use App\Models\TaskStatus;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\TaskStatusResource\Pages;
+use App\Filament\Resources\TaskStatusResource\RelationManagers;
+
+class TaskStatusResource extends Resource
+{
+    protected static ?string $model = TaskStatus::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->sortable()->searchable()
+                    ->wrap(),
+                TextColumn::make('created_at')
+                    ->date()
+                    ->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTaskStatuses::route('/'),
+            'create' => Pages\CreateTaskStatus::route('/create'),
+            'edit' => Pages\EditTaskStatus::route('/{record}/edit'),
+        ];
+    }
+}
