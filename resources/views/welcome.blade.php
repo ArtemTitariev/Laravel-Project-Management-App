406d6c6d41e9248bfa0a30d0f8ebc3073fce44ec
Artem Titariev <artem.titariev@gmail.com>
2024-02-07 22:39:26 +0200

    welcome page: code cleanup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,69 +34,51 @@
             <section class="pb-10 bg-blueGray-200 -mt-24">
                 <div class="container mx-auto px-4">
                     <div class="flex flex-wrap">
-                        <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
-                            <div
-                                class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                                <div class="px-4 py-5 flex-auto">
-                                    <div
-                                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-amber-400">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
-                                        </svg>
-
-                                    </div>
-                                    <h6 class="text-xl font-semibold">Projects</h6>
-                                    <p class="mt-2 mb-4 text-blueGray-500">
-                                        {{ __('In the Projects section, you can view and manage active projects.
-                                                                                                                            Add new projects, edit information, and track the status of each project.') }}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="w-full md:w-4/12 px-4 text-center">
-                            <div
-                                class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                                <div class="px-4 py-5 flex-auto">
-                                    <div
-                                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
-                                        </svg>
-
-                                    </div>
-                                    <h6 class="text-xl font-semibold">Tasks</h6>
-                                    <p class="mt-2 mb-4 text-blueGray-500">
-                                        {{ __('The Tasks section provides a convenient way to view and manage your workload.
-                                                                                                                        Create new tasks, assign them to employees, and track their progress.') }}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="pt-6 w-full md:w-4/12 px-4 text-center">
-                            <div
-                                class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                                <div class="px-4 py-5 flex-auto">
-                                    <div
-                                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
-                                        </svg>
-                                    </div>
-                                    <h6 class="text-xl font-semibold">Employees and Teams</h6>
-                                    <p class="mt-2 mb-4 text-blueGray-500">
-                                        {{ __('In the Employees and Teams sections, you can view a list of employees, their roles, and assigned tasks.
-                                                                                                                        Add new employees and form working groups for effective project management.') }}
-                                    </p>
-
-                                </div>
-                            </div>
-                        </div>
+                        <x-welcome.data-section class="lg:pt-12 pt-6" icon-bg="bg-amber-400">
+                            <x-slot:title>
+                                {{ __('Projects') }}
+                            </x-slot>
+                            <x-slot:description>
+                                {{ __('In the Projects section, you can view and manage active projects. Add new projects, edit information, and track the status of each project.') }}
+                            </x-slot>
+                            <x-slot:icon>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+                                </svg>
+                            </x-slot>
+                        </x-welcome.data-section>
+                        <x-welcome.data-section icon-bg="bg-lightBlue-400">
+                            <x-slot:title>
+                                {{ __('Tasks') }}
+                            </x-slot>
+                            <x-slot:description>
+                                {{ __('The Tasks section provides a convenient way to view and manage your workload. Create new tasks, assign them to employees, and track their progress.') }}
+                            </x-slot>
+                            <x-slot:icon>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                                </svg>
+                            </x-slot>
+                        </x-welcome.data-section>
+                        <x-welcome.data-section icon-bg="bg-green-400" class="pt-6">
+                            <x-slot:title>
+                                {{ __('Employees and Teams') }}
+                            </x-slot>
+                            <x-slot:description>
+                                {{ __('In the Employees and Teams sections, you can view a list of employees, their positions, and assigned tasks. Add new employees and form working groups for effective project management.') }}
+                            </x-slot>
+                            <x-slot:icon>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                                </svg>
+                            </x-slot>
+                        </x-welcome.data-section>
                     </div>
 
                     <footer class="relative  pt-8 pb-6 mt-1">
@@ -105,7 +87,8 @@
                                 <div class="w-full md:w-6/12 px-4 mx-auto text-center">
                                     <div class="text-sm text-blueGray-500 font-semibold py-1">
                                         {{ __('Developed by') }} <a href="https://github.com/ArtemTitariev"
-                                            class="text-blueGray-500 hover:text-blueGray-800" target="_blank"> Artem
+                                            class="text-blueGray-500 hover:text-blueGray-800 underline" target="_blank">
+                                            Artem
                                             Titariev</a>
                                     </div>
                                 </div>
